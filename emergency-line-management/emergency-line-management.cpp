f4e11237897e29d44a4efc5763ead3b54d6caadd
Berkay <mberkayermis@gmail.com>
2021-01-03 07:06:57 +0300

    Main menu and fire department menu were created

--- emergency-line-management/emergency-line-management.cpp
+++ emergency-line-management/emergency-line-management.cpp
@@ -2,9 +2,47 @@
 
 using namespace std;
 
+//Fire department menu
+void fireMenu() {
+	int fireChoose;
+	cout << "1 >> Emergency fire notice " << endl;
+	cout << "2 >> Forest fire notice" << endl;
+f:
+	cout << "Enter your choice: ";
+	cin >> fireChoose;
+
+	if (fireChoose > 2 || fireChoose < 1) {
+		cout << "Invalid Choice\n";
+		cout << "Try again...........\n\n";
+		goto f;
+	}
+
+}
+
 int main()
 {
+
+	int choose;
+	//Main menu
 	cout << "Welcome to Emergency Call Lines" << endl;
-	cout << "A" << endl;
-	cout << "B" << endl;
+	cout << "1 >> Emergency" << endl;
+	cout << "2 >> Police" << endl;
+	cout << "3 >> Fire Department" << endl;
+	cout << "4 >> Veterinary service" << endl;
+	cout << "5 >> Police" << endl;
+
+	
+a:
+	cout << "Enter your choice: ";
+	cin >> choose;
+	if (choose > 5 || choose < 1) { 
+		cout << "Invalid Choice\n";
+		cout << "Try again...........\n\n";
+		goto a; 
+	} 
+	if (choose == 3) {
+		fireMenu();
+	}
+
+
 }
